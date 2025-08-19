7e7fdaf21f90308189121016e71055941a6269c9
vbozaric <vasilije.bozaric@dinitcs.com>
2025-08-19 15:55:40 +0200

    solve katis flippingPatties

---
+++ katis/flippingpatties.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<tuple>
+#include<vector>
+#include<cmath>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> max(50000,0);
+    vector<tuple<int, int>>  patty(n);
+    int currentMax = 0;
+    for (int i = 0; i < n; i++) {
+        int a,b;
+        cin >> a >> b;
+        max[b]++;
+        max[b-a]++;
+        max[b-(2*a)]++;
+    }
+    for (int i = 0; i < 50000; i++) {
+     if (max[i]>currentMax) {
+         currentMax = max[i];
+     }
+    }
+    cout << (currentMax + 1)/2;
+}
